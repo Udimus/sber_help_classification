4a104a337117c84a0a081f6ee7732d8b00c83aba
udimus <dandi007@yandex.ru>
2020-06-12 20:00:16 +0300

    Check model save-load.

--- module/prepare_data.py
+++ module/prepare_data.py
@@ -6,7 +6,6 @@ import logging
 from string import punctuation
 
 import pandas as pd
-import numpy as np
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import RussianStemmer
